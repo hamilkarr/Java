8b0c33ba0f17df3062948d50fddeaccf8509c0bb
hamilkarr <12uclock@gmail.com>
2021-09-26 16:25:44 +0900

    챕터 12 워크스페이스및 파일정리

---
+++ Java_source/SecondStudyPartTwo/src/chapter12Collection/sub4/GenericPrinter1Test.java
@@ -0,0 +1,7 @@
+package chapter12Collection.sub4;
+
+public class GenericPrinter1Test {
+  public static void main(String[] args) {
+    System.out.println("잘되니??");
+  }
+}
---
+++ Java_source/SecondStudyPartTwo/src/chapter12Collection/sub4/Plastic1.java
@@ -0,0 +1,17 @@
+package chapter12Collection.sub4;
+
+public class Plastic1 {
+  public void doPrinting() {
+    System.out.println("Plastic1로 출력합니다.");
+  }
+
+  @Override
+  public String toString() {
+    return "재료는 Plastic1 입니다.";
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println("진짜안되??");
+  }
+}
